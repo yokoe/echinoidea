ce872f95d37ba0854aeb0d82b022091c0eb1acde
Sota Yokoe <sota@pankaku.com>
2012-10-03 10:30:51 +0900

    Create Editor directory if not exist

--- lib/echinoidea.rb
+++ lib/echinoidea.rb
@@ -4,4 +4,16 @@ module Echinoidea
   def self.project_root_directory?(path)
   	File.exists?([path, "Assets"].join("/"))
   end
+
+  def self.editor_directory_path(root_directory_path)
+  	[root_directory_path, "Assets", "Editor"].join("/")
+  end
+
+  def self.editor_directory_exists?(path)
+  	File.exists?(self.editor_directory_path(path))
+  end
+
+  def self.create_editor_directory(path)
+  	Dir::mkdir(self.editor_directory_path(path))
+  end
 end
